da91f7bb88cf091b498aa28dce5511d84d639d5a
Roger <roger.juarez.rdjjm@gmail.com>
2020-11-23 10:58:33 -0600

    Login y registro

--- src/app/iniciar/iniciar.component.ts
+++ src/app/iniciar/iniciar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { EMPTY } from 'rxjs';
+
+import  Swal  from 'sweetalert2';
 
 @Component({
   selector: 'app-iniciar',
@@ -7,9 +11,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IniciarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
+
+  gohome(){
+    event.preventDefault()
+    var email = (<HTMLInputElement>document.getElementById("email")).value
+    var contra =(<HTMLInputElement>document.getElementById("password")).value
+
+    if(email == ""){
+      document.getElementById("email").focus();
+      Swal.fire('Debes ingresar el correo','','error')
+    }else if(contra == ""){
+      document.getElementById("password").focus();
+      Swal.fire('Debes ingresar la contraseña','','error')
+      
+    }else{
+      if(email=="hola123@gmail.com" && contra=="1234"){
+        this.router.navigate(['/explorar'])
+        Swal.fire({
+          position: 'center',
+          icon: 'success',
+          title: 'Logeado!',
+          showConfirmButton: false,
+          timer: 1500
+        })
+      }else{
+        Swal.fire('Datos Incorrectos!','Revisa bien el correo o la contraseña','error')
+      }
+    }
+  }
+
 
 }
--- src/app/registrar/registrar.component.ts
+++ src/app/registrar/registrar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { EMPTY } from 'rxjs';
+import  Swal  from 'sweetalert2';
 
 @Component({
   selector: 'app-registrar',
@@ -7,9 +10,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
+
+
+  registrado(): void {
+    event.preventDefault()
+    var nombre = (<HTMLInputElement>document.getElementById("nombrec")).value
+    var apellido = (<HTMLInputElement>document.getElementById("apellidoc")).value
+    var email = (<HTMLInputElement>document.getElementById("email")).value
+    var contra1 = (<HTMLInputElement>document.getElementById("password")).value
+
+
+    if(nombre == ""){
+      document.getElementById("nombrec").focus();
+      Swal.fire('Debes ingresar tu nombre','','error')
+
+    }else if(apellido == ""){
+      document.getElementById("apellidoc").focus();
+      Swal.fire('Debes ingresar tu Apellido','','error')
+
+    }else if(email == ""){
+      document.getElementById("email").focus();
+      Swal.fire('Debes ingresar el correo','','error')
+
+    
+    }else if(contra1 == ""){
+      document.getElementById("password").focus();
+      Swal.fire('Debes ingresar la contraseña','','error')
+
+    } else{
+      if(contra1 !=''){
+        this.router.navigate(['/explorar'])
+        Swal.fire({
+          position: 'center',
+          icon: 'success',
+          title: 'Registrado & Logeado!',
+          showConfirmButton: false,
+          timer: 1500
+        })
+      }else{
+        Swal.fire('Datos Incorrectos!','Las contraseñas no coinciden','error')
+      }
+    }
+    }
+    
+    
+
+
+  
+  
 
 }
